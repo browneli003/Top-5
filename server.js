68a20c2326572516e44a0186b7f8a5946abccac9
unknown <boedi15@gmail.com>
2021-09-19 16:11:01 -0600

    added handlebars to server

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({})
 // import sequelize connection
 const sequelize = require('./config/connection');
 
@@ -9,6 +11,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 // app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
@@ -17,3 +22,10 @@ sequelize.sync({ force: false }).then(() => {
     console.log(`App listening on port ${PORT}!`);
   });
 });
+
+app.listen(8080, () => {
+  console.log('Server is starting at port ', 8080);
+} );
+
+;
+
