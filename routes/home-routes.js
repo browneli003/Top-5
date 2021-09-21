378d66badc1591375a568e45ebfb9247d7c3355f
Sean <sdillon215@gmail.com>
2021-09-21 13:39:29 -0700

    login page added

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,8 +1,14 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 
+// render home page
 router.get('/', (req, res) => {
   res.render('homepage');
+});
+
+// render login page
+router.get('/login', (req, res) => {
+  res.render('login');
 });
 
 module.exports = router;
