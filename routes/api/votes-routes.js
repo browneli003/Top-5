c6b8764c4b1c4779d91de3c954de06476d64813d
Elia Brown <brownelijah143@gmail.com>
2021-09-20 20:27:42 -0600

    vote routes created and tested

--- routes/api/votes-routes.js
+++ routes/api/votes-routes.js
@@ -33,15 +33,15 @@ router.get('/:id', (req, res) => {
             'item_name'
         ],
         include: [
-            {
+        {
                 model: User,
                 attributes: ['id']
-            },
-            {
+        },
+        {
                 model: Topics,
-                attributes: ['id', 'topic', 'vote_tally', 'user_id', 'rank', 'item_name']
-            }
-        ]
+                attributes: ['id', 'topic', 'vote_tally', 'user_id']
+        }    
+        ]  
     })
     .then(dbVotesData => res.json(dbVotesData))
     .catch(err => {
@@ -50,8 +50,39 @@ router.get('/:id', (req, res) => {
     });
 });
 
+router.post('/', (req, res) => {
+    Votes.create({
+        id: req.body.id,
+        topic_id: req.body.topic_id,
+        user_id: req.body.user_id,
+        rank: req.body.rank,
+        item_name: req.body.item_name
+    })
+    .then(dbVotesData => res.json(dbVotesData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
-
+router.delete('/:id', (req, res) => {
+    Votes.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbVotesData => {
+    if (!dbVotesData) {
+        res.status(404).json({message: 'Vote not recorded!'});
+        return;
+    }
+    res.json(dbVotesData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 
 module.exports = router;
