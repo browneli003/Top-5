c005c937c26a60eb68e3c0f9022469f796eab754
Sean <sdillon215@gmail.com>
2021-09-20 17:03:36 -0700

    post user routes edit

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -46,20 +46,19 @@ router.post('/', (req, res) => {
     email: req.body.email,
     password: req.body.password
   })
-    .then(dbUserData => {
-      if (!dbUserData) {
-        res.status(404).json({ message: 'No user found with this id' });
-        return;
-      }
-      req.session.save(() => {
-        req.session.user_id = dbUserData.id;
-        req.session.username = dbUserData.username;
-        req.session.loggedIn = true;
-        res.json(dbUserData);
-
-      })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
     });
-});
+  });
+  // req.session.save(() => {
+  //   req.session.user_id = dbUserData.id;
+  //   req.session.username = dbUserData.username;
+  //   req.session.loggedIn = true;
+  //   res.json(dbUserData);
+
+  // })
 
 router.post('/login', (req, res) => {
   // expects {email: 'lernantino@gmail.com', password: 'password1234'}
