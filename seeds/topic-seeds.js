9255936dc50ad0876a63b5873c69cd9129a22b5f
Sean <sdillon215@gmail.com>
2021-10-04 23:44:12 -0700

    seeds edited

--- seeds/topic-seeds.js
+++ seeds/topic-seeds.js
@@ -32,14 +32,14 @@ const topicData = [
     user_id: 1
   },
   {
-    topic: 'Top 5 Burritos',
-    item1: 'Chile Verde',
-    item2: 'Chicken',
-    item3: 'pork',
-    item4: 'veggie',
-    item5: 'no borrito',
-    vote_tally: 22,
-    user_id: 2
+    topic: 'Top 5 Trucks',
+    item1: 'Tacoma',
+    item2: 'Tacoma',
+    item3: 'Tacoma',
+    item4: 'Tacoma',
+    item5: 'Tacoma',
+    vote_tally: 6000000,
+    user_id: 4
   }
 ];
 
